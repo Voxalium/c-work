dc80cc84e25c0031371e2e972c4cb7bfe582ed56
Voxalium <voxalium@gmail.com>
2023-04-06 16:47:32 +0200

    mode major and minor

--- scale.c
+++ scale.c
@@ -1,6 +1,3 @@
-/* Exercice from "The Audio Programming Book"
-   for printing the notes of a selected major scale;  
- */
 #include <stdio.h>
 #include <string.h>
 
@@ -17,44 +14,15 @@ int main(){
     printf("Please enter the key, eg. Eb : ");
     scanf("%s", key);
 
-//Check key
-    for(int i = 0; i < 12; i++){
-            if(strcmp(scale[i], key) == 0){
-                printf("OK");
-                break;
-            }
-            else{
-                printf("Invalid key");
-                return 1;
-            }
-        }
-
-    printf("Pleass enter the mode eg, 1 : ");
+    printf("Mode : ");
     scanf("%i", &mode);
-    
-//Check mode    
+   
     if(mode < 1 || mode > 7){
         printf("%i: invalid mode", mode);
         return 1;
     }
     
-    
-
-
-    switch (mode)
-    {
-    case 1:
-        printf("== %s major scale ==\n", key);
-        break;
-    case 2:
-        printf("== %s minor scale ==\n", key);
-        break;    
-    default:
-        printf("%s: invalid key\n", key);
-        return 1;
-    }
-
-   /*  if(mode ==  "1"){
+    if(mode ==  1){
 
         for(int i=0; i<12; i++){
 
@@ -68,7 +36,7 @@ int main(){
         if(note >= 0){
             for (int i=0; i<7 ; i++){
                 printf("%s ", scale[note%12]);
-                if(i !=1) note +=2;
+                if(i !=2) note +=2;
                 else note++;
             }
             printf("\n");
@@ -82,7 +50,7 @@ int main(){
 
     }
 
-    if(mode == "2"){
+    if(mode == 2){
         for(int i=0; i<12; i++){
 
             if(strcmp(scale[i], key) == 0){
@@ -95,18 +63,17 @@ int main(){
     if(note >= 0){
         for (int i=0; i<7 ; i++){
             printf("%s ", scale[note%12]);
-            if(i !=1 && i !=5) note +=2;
+            if(i !=1 && i !=4) note +=2;
             else note++;
         }
         printf("\n");
         return 0;        
-        }
-    
+        }    
     else{
         printf("%s: invalid key\n", key);
         return 1;
     }
         
     }
- */
+
 }
